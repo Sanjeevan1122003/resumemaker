0d5cede3468077862c8c0cf62ec995539f84c844
sanjeevan <145178642+Sanjeevan1122003@users.noreply.github.com>
2025-01-26 22:19:31 +0530

    app.js

--- api/app.js
+++ api/app.js
@@ -44,7 +44,7 @@ app.get("/login/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
-app.post("/api/signup/", async (req, res) => {
+app.post("/usersignup/", async (req, res) => {
     const data = req.body;
     const { firstname, surname, username, gender, email, password } = data;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -54,7 +54,7 @@ app.post("/api/signup/", async (req, res) => {
     res.send("Signup successful!");
 })
 
-app.post("/api/login/", async (req, res) => {
+app.post("/userlogin/", async (req, res) => {
     const data = req.body;
     const { username, password } = data;
     const query = `SELECT * FROM users_credentials WHERE username = ?`;
