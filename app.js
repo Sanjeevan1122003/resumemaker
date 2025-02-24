5ec9405985c7c4cb6e4318184258ef397782714c
sanjeevan <145178642+Sanjeevan1122003@users.noreply.github.com>
2025-02-24 16:12:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.get("/logout/", (req, res) => {
 
 // ✅ Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/`);
+  console.log(`Server is running on ${PORT}`);
 });
 
 module.exports = app;
